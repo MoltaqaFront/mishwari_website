137bccd1cea3842da374cd05b03cc3da1c1f713c
shadyaabed <shady@moltaqa.net>
2023-11-26 09:56:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mishwari_website
---
+++ locales/en-US.js
@@ -0,0 +1,65 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About Mishwari',
+      title: 'Providing reliable and suitable transportation solutions for everyone',
+      desc: 'Welcome to Mishwari, the new ride-hailing app. We offer you a unique and convenient experience to meet your mobility needs. Whether you need an individual or a group trip, Mishwari is the perfect solution for you.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: "Easy and convenient booking",
+      desc: 'Book your flight easily and conveniently using the app. Select your current location and desired destination and choose your preferred type of vehicle.',
+    },
+    {
+      image: '2.svg',
+      title: "Wide range of car options",
+      desc: "Benefit from the wide variety of cars available in the Mishwari application. Whether you prefer a luxury car, taxi or community bus, we cater to all your needs.",
+    },
+    {
+      image: '3.svg',
+      title: "Trip tracking in real time",
+      desc: 'See the drivers location and track your trip in real time with GPS. This ensures that you get accurate information about your expected arrival time.'
+    },
+  ],
+  about: {
+    mainTitle: 'About Mishwari',
+    title: 'Providing reliable and suitable transportation solutions for everyone',
+    desc: "<p>Welcome to Mishwari, the new ride-hailing app. We offer you a unique and convenient experience to meet your mobility needs. Whether you need an individual or a group trip, Mishwari is the perfect solution for you.</p>",
+  },
+  downloadText: "<p>You can download the Mishwari app now by visiting the App Store on your smartphone, searching for Mishwari, and then downloading the app on your smartphone.</p>",
+  isoLink: "",
+  androidLink: "",
+  address: 'KSA - Jeddah',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved For Moltaqa Software House',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+    },
+}
